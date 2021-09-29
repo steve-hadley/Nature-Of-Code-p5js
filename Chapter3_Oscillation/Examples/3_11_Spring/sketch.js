e60de5f0bfc022c56762bab11c87593cd695938f
Steve Hadley <steven.dale.hadley@gmail.com>
2021-09-29 16:29:09 +1000

    Tweaking spring example

--- Chapter3_Oscillation/Examples/3_11_Spring/sketch.js
+++ Chapter3_Oscillation/Examples/3_11_Spring/sketch.js
@@ -1,21 +1,22 @@
 let spring;
 let bob;
-let gravity; 
+let restLength = 200;
 
 function setup() {
   createCanvas(800, 600);
-  bob = new Bob(createVector(width / 2, height / 2));
-  spring = new Spring(createVector(width/2, height / 2), bob);
-  gravity = createVector(0, 1);
+  bob = new Bob(createVector(width / 2, restLength));
+  spring = new Spring(createVector(width/2, 0), bob, restLength);
 }
 
 function draw() {
   background(220);
+
+  let gravity = createVector(0, 1);
   bob.applyForce(gravity);
+
   if(mouseIsPressed){
     bob.position = createVector(mouseX, mouseY);
-  }
-  else{
+  }else{
     spring.connect(bob);
   }
   bob.update();
@@ -24,16 +25,16 @@ function draw() {
 }
 
 class Spring {
-    constructor(anchor, bob) {
+    constructor(anchor, bob, restLength) {
       this.anchor = anchor;
       this.bob = bob;
-      this.k = 0.05;
-      this.length = 1;
+      this.k = 0.01;
+      this.restLength = restLength;
     }
     connect(bob){
       let force = p5.Vector.sub(bob.position, this.anchor);
       let d = force.mag();
-      let displacement = d - this.length;
+      let displacement = d - this.restLength;
       force.normalize();
       force.mult(-1 * this.k * displacement);
       bob.applyForce(force);
